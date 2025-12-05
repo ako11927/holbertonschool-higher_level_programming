58e995206e060f75532b6460250d51d46d1b676c
ako11927 <11927@holbertonstudents.com>
2025-12-05 11:34:54 +0000

    fixing the two hidden tests

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,7 +5,7 @@ Module add_integer
 
 
 def add_integer(a, b=98):
-    """Adds two integers"""
+    """Adds two integers."""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
