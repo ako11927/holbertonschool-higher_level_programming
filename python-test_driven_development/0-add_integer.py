d2c16e507832d7dd2ee8bb93bf3deff747d4b9b6
ako11927 <11927@holbertonstudents.com>
2025-12-05 12:35:55 +0000

    try to get full answer by fix pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,14 +4,11 @@
 
 def add_integer(a, b=98):
     """Add two integers.
-    
     Args:
         a: First number, must be integer or float
         b: Second number, must be integer or float, defaults to 98
-        
     Returns:
         The sum of a and b as an integer
-        
     Raises:
         TypeError: If a or b is not an integer or float
     """
@@ -19,7 +16,6 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    
     try:
         return int(a) + int(b)
     except OverflowError:
