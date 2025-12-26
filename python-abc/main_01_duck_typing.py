1b7f62d77b6a6550e86cb0f4331345a3e0a5f74a
ako11927 <11927@holbertonstudents.com>
2025-12-26 19:23:05 +0000

    Fix Task 1: Clean formatting and proper implementation

--- python-abc/main_01_duck_typing.py
+++ python-abc/main_01_duck_typing.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 from task_01_duck_typing import Circle, Rectangle, shape_info
 
-circle = Circle(radius=5)
-rectangle = Rectangle(width=4, height=7)
+class TestDuck:
+    def area(self):
+        return 123
+    def perimeter(self):
+        return 456
 
-shape_info(circle)
-shape_info(rectangle)
+td = TestDuck()
+shape_info(td)
+"
