5f27de8b7c0490285e3e6d39d6718e85a23be042
ako11927 <11927@holbertonstudents.com>
2025-12-26 19:14:44 +0000

    Fix formatting and indentation for task 1

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,25 +1,52 @@
 #!/usr/bin/python3
+"""Task 1: Shapes, Interfaces, and Duck Typing"""
+
 from abc import ABC, abstractmethod
 import math
 
+
 class Shape(ABC):
+    """Shape abstract class"""
+    
     @abstractmethod
-    def area(self): pass
+    def area(self):
+        """Return area"""
+        pass
+    
     @abstractmethod
-    def perimeter(self): pass
+    def perimeter(self):
+        """Return perimeter"""
+        pass
+
 
 class Circle(Shape):
-    def __init__(self, radius): self.radius = radius
-    def area(self): return math.pi * self.radius ** 2
-    def perimeter(self): return 2 * math.pi * self.radius
+    """Circle class"""
+    
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def area(self):
+        return math.pi * self.radius ** 2
+    
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
 
 class Rectangle(Shape):
+    """Rectangle class"""
+    
     def __init__(self, width, height):
         self.width = width
         self.height = height
-    def area(self): return self.width * self.height
-    def perimeter(self): return 2 * (self.width + self.height)
+    
+    def area(self):
+        return self.width * self.height
+    
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
 
 def shape_info(shape):
-    print('Area:', shape.area())
-    print('Perimeter:', shape.perimeter())
+    """Print area and perimeter"""
+    print("Area: {}".format(shape.area()))
+    print("Perimeter: {}".format(shape.perimeter()))
