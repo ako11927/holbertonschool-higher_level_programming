c53da40f7e7f4d24e67c25304ecf974010c31882
ako11927 <11927@holbertonstudents.com>
2025-12-26 18:44:07 +0000

    Complete Python ABC

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,53 +1,31 @@
 #!/usr/bin/python3
-"""Task 1: Shapes, Interfaces, and Duck Typing"""
+"""Task 1"""
 
 from abc import ABC, abstractmethod
 import math
 
 
 class Shape(ABC):
-    """Shape abstract class"""
-    
     @abstractmethod
-    def area(self):
-        pass
-    
+    def area(self): pass
     @abstractmethod
-    def perimeter(self):
-        pass
+    def perimeter(self): pass
 
 
 class Circle(Shape):
-    """Circle class"""
-    
-    def __init__(self, radius):
-        self.radius = radius
-    
-    def area(self):
-        return math.pi * self.radius ** 2
-    
-    def perimeter(self):
-        return 2 * math.pi * self.radius
+    def __init__(self, radius): self.radius = radius
+    def area(self): return math.pi * self.radius ** 2
+    def perimeter(self): return 2 * math.pi * self.radius
 
 
 class Rectangle(Shape):
-    """Rectangle class"""
-    
     def __init__(self, width, height):
         self.width = width
         self.height = height
-    
-    def area(self):
-        return self.width * self.height
-    
-    def perimeter(self):
-        return 2 * (self.width + self.height)
+    def area(self): return self.width * self.height
+    def perimeter(self): return 2 * (self.width + self.height)
 
 
 def shape_info(shape):
-    """Print area and perimeter - MUST use duck typing"""
-    # Don't check type, just call methods
-    area = shape.area()
-    perimeter = shape.perimeter()
-    print("Area: {}".format(area))
-    print("Perimeter: {}".format(perimeter))
+    print("Area:", shape.area())
+    print("Perimeter:", shape.perimeter())
