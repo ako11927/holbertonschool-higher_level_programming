03f1f991c8c306f5334c39c5f3ffeee1c918b96a
root <11927@holbertonstudents.com>
2025-12-27 18:44:12 +0000

    Complete Python serialization tasks

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,18 @@
+import csv
+import json
+
+def convert_csv_to_json(filename):
+    try:
+        with open(filename, "r") as csv_file:
+            reader = csv.DictReader(csv_file)
+            data = list(reader)
+        
+        with open("data.json", "w") as json_file:
+            json.dump(data, json_file)
+        
+        return True
+    
+    except FileNotFoundError:
+        return False
+    except Exception:
+        return False
