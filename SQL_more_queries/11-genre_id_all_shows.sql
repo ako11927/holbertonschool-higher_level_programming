9a9796e913caf2aa8876bcdd92ef46973dc4da6c
ako11927 <11927@holbertonstudents.com>
2026-01-23 21:08:21 +0000

    Fix tasks 10, 11, 13, 16

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,2 +1,2 @@
 -- Count shows per genre
-SELECT tv_show_genres.genre_id, COUNT(tv_show_genres.genre_id) AS number_of_shows FROM tv_shows RIGHT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id GROUP BY tv_show_genres.genre_id ORDER BY number_of_shows DESC;
+SELECT tv_genres.name, COUNT(tv_show_genres.genre_id) AS number_of_shows FROM tv_show_genres INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
