25e3f30e58a9331ce0fd6d218586af0925c0c11d
ako11927 <11927@holbertonstudents.com>
2026-01-23 20:51:05 +0000

    Task 3 final

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
--- Create force_name table
-CREATE TABLE IF NOT EXISTS force_name (name VARCHAR(256) NOT NULL);
+-- Create force_name table with id and name columns
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
