597fb6f9711eb8d84a9ef5f5cb349f50f715211b
ako11927 <11927@holbertonstudents.com>
2026-01-27 10:05:42 +0000

    doing some adjusting

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     # Execute the query using string formatting
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC".format(
         state_name)
     cursor.execute(query)
 
