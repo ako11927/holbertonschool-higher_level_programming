6b994619b762da6b789c6b4f9a43732ac0550b33
ako11927 <11927@holbertonstudents.com>
2026-01-27 10:27:29 +0000

    answering for the rest of tasks

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -13,6 +13,6 @@ class State(Base):
     State class that links to MySQL table 'states'
     """
     __tablename__ = 'states'
-    
+
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
